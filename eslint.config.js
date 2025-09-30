401cedc1bd20cfd5b12634cbf2a36b93f4ab07d8
Gamzee <lithiumturkin@protonmail.com>
2025-09-30 13:34:05 +0200

    Feat: Change qoutations in formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    ignores: ["dist/*"],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true, // Enforce single quotes
+          // You can add other Prettier options here too
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/*'],
   },
 ]);
