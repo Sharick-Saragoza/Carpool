3d46f6cafb846aade5dd7b7b48f7c76c61c717db
Gamzee <lithiumturkin@protonmail.com>
2025-10-10 11:46:41 +0200

    install native paper

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,18 @@
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
 
   return {
     presets: [['babel-preset-expo'], 'nativewind/babel'],
-
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel']
+      }
+    },
     plugins: [
       [
         'module-resolver',
         {
           root: ['./'],
-
           alias: {
             '@': './',
             'tailwind.config': './tailwind.config.js',
