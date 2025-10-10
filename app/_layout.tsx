63648195477f3fd6ac9c78f77f805546c31a289c
Gamzee <lithiumturkin@protonmail.com>
2025-10-10 12:43:37 +0200

    Feat: install paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+
+import React = require('react');
 
 export default function RootLayout() {
-    return (
-        <GluestackUIProvider>
-            <Stack>
-                <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-                <Stack.Screen name='index' options={{ headerShown: false }} />
-            </Stack>
-        </GluestackUIProvider>
-    );
+  return (
+    <PaperProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </PaperProvider>
+  );
 }
