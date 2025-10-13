ea7674e8c29523e400acc744962aec214e4caa5c
Gamzee <lithiumturkin@protonmail.com>
2025-10-13 12:02:16 +0200

    Fix: port all pages to scrollview

--- app/(tabs)/drive.tsx
+++ app/(tabs)/drive.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
-import { Button, Card, IconButton, Text } from 'react-native-paper';
+import { Image, ScrollView, View } from 'react-native';
+import { Card, IconButton, Text } from 'react-native-paper';
 
 export default function MakeCarpool() {
   return (
-    <View className="flex-1 p-4">
-      {/* Plus button */}
-      <IconButton
-        icon="plus"
-        mode="contained"
-        size={20}
-        iconColor="white"
-        containerColor="#2563eb"
-        onPress={() => console.log('Pressed')}
-      />
+    <ScrollView>
+      <View className="flex-1 p-4">
+        {/* Plus button */}
+        <IconButton
+          icon="plus"
+          mode="contained"
+          size={20}
+          iconColor="white"
+          containerColor="#2563eb"
+          onPress={() => console.log('Pressed')}
+        />
 
-      {/* Carpool Card */}
-      <CarpoolCard
-        profileImage="https://randomuser.me/api/portraits/men/32.jpg"
-        from="Amsterdam"
-        to="Utrecht"
-        time="08:00 - 09:00"
-      />
-    </View>
+        {/* Carpool Card */}
+        <CarpoolCard
+          profileImage="https://randomuser.me/api/portraits/men/32.jpg"
+          from="Amsterdam"
+          to="Utrecht"
+          time="08:00 - 09:00"
+        />
+      </View>
+    </ScrollView>
   );
 }
 
