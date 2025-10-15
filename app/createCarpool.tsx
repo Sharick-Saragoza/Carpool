2467e952fe5503faaa504e953cb28e79c2f6253e
Gamzee <lithiumturkin@protonmail.com>
2025-10-15 11:53:42 +0200

    Feat: add initial date picker

--- app/createCarpool.tsx
+++ app/createCarpool.tsx
@@ -1,6 +1,8 @@
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { SearchIcon } from 'lucide-react-native';
 import { useState } from 'react';
 import { ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
@@ -8,68 +10,92 @@ import { View } from '@/components/ui/view';
 
 export default function CreateCarpool() {
   const [page, setPage] = useState<number>(1);
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [show, setShow] = useState(false);
+  const [mode, setMode] = useState('date');
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode('date')
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
 
   const renderContent = () => {
     if (page === 1) {
       return (
         <ScrollView>
           <View>
-            <Card className="flex-1">
+            <Card className='flex-1'>
               <Input>
-                <InputSlot className="pl-3">
+                <InputSlot className='pl-3'>
                   <InputIcon as={SearchIcon} />
                 </InputSlot>
-                <InputField placeholder="Voer het volledige address in" />
+                <InputField placeholder='Voer het volledige address in' />
               </Input>
             </Card>
 
             <Card>
               <Input>
-                <InputSlot className="pl-3">
+                <InputSlot className='pl-3'>
                   <InputIcon as={SearchIcon} />
                 </InputSlot>
-                <InputField placeholder="Voer het volledige address in" />
+                <InputField placeholder='Voer het volledige address in' />
               </Input>
             </Card>
 
             <Card>
               <Input>
-                <InputSlot className="pl-3">
+                <InputSlot className='pl-3'>
                   <InputIcon as={SearchIcon} />
                 </InputSlot>
-                <InputField placeholder="Voer het volledige address in" />
+                <InputField placeholder='Voer het volledige address in' />
               </Input>
             </Card>
           </View>
         </ScrollView>
       );
-    } 
+    }
 
     if (page === 2) {
       return (
-        <View className="flex-1">
-          <Card>
-              <Input>
-                <InputSlot className="pl-3">
-                  <InputIcon as={SearchIcon} />
-                </InputSlot>
-                <InputField placeholder="Voer het volledige address in2" />
-              </Input>
-          </Card>
+        <View className='flex-1'>
+          <SafeAreaView>
+            <Button onPress={showDatepicker}>
+              <ButtonText>show time</ButtonText>
+            </Button>
+            {show && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                onChange={onChange}
+              />
+            )}
+          </SafeAreaView>
         </View>
       );
     }
 
     if (page === 3) {
       return (
-        <View className="flex-1">
+        <View className='flex-1'>
           <Card>
-              <Input>
-                <InputSlot className="pl-3">
-                  <InputIcon as={SearchIcon} />
-                </InputSlot>
-                <InputField placeholder="Voer het volledige address in3" />
-              </Input>
+            <Input>
+              <InputSlot className='pl-3'>
+                <InputIcon as={SearchIcon} />
+              </InputSlot>
+              <InputField placeholder='Voer het volledige address in3' />
+            </Input>
           </Card>
         </View>
       );
@@ -77,19 +103,16 @@ export default function CreateCarpool() {
   };
 
   return (
-    <View className="flex-1">
+    <View className='flex-1'>
       {renderContent()}
 
-      <View className="bg-gray-400 p-4 flex flex-row gap-5">
+      <View className='bg-gray-400 p-4 flex flex-row gap-5'>
         {page !== 1 && (
-          <Button
-            className="flex-1"
-            onPress={() => setPage(page - 1)}
-          >
+          <Button className='flex-1' onPress={() => setPage(page - 1)}>
             <ButtonText>Back</ButtonText>
           </Button>
         )}
-        <Button className="flex-1" onPress={() => setPage(page + 1)}>
+        <Button className='flex-1' onPress={() => setPage(page + 1)}>
           <ButtonText>next</ButtonText>
         </Button>
       </View>
