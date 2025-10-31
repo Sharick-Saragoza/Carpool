f003fcf6e3bb633b26172a67a5c2a3883bf22e73
Hamid-xb <120631726+Hamid-xb@users.noreply.github.com>
2025-10-31 14:16:46 +0100

    front end explore pagina, spacing probleem gefixt, dateTimePicker toegevoegd. veranderingen op andere paginas gefixt

--- app/(tabs)/drive.tsx
+++ app/(tabs)/drive.tsx
@@ -4,7 +4,6 @@ import { ScrollView } from 'react-native';
 import { CarpoolCard } from '@/components/CarpoolCard';
 import { Button, ButtonIcon, ButtonText } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
-import ScreenWrapper from '@/components/ScreenWrapper';
 
 export default function MakeCarpool() {
   const handleCreateCarpool = () => {
@@ -12,27 +11,25 @@ export default function MakeCarpool() {
   };
 
   return (
-    <ScreenWrapper>
-      <ScrollView>
-        <Card className="flex-1 m-3 bg-gray-400 p-0">
-          <Card className="bg-gray-500 m-0">
-            <Button
-              size="lg"
-              onPress={handleCreateCarpool}
-            >
-              <ButtonIcon as={Plus} />
-              <ButtonText>Maak een Carpool aan</ButtonText>
-            </Button>
-          </Card>
-
-          <CarpoolCard
-            profileImage="https://randomuser.me/api/portraits/men/32.jpg"
-            from="Amsterdam"
-            to="Utrecht"
-            time="08:00 - 09:00"
-          />
+    <ScrollView>
+      <Card className="flex-1 m-3 bg-gray-400 p-0">
+        <Card className="bg-gray-500 m-0">
+          <Button
+            size="lg"
+            onPress={handleCreateCarpool}
+          >
+            <ButtonIcon as={Plus} />
+            <ButtonText>Maak een Carpool aan</ButtonText>
+          </Button>
         </Card>
-      </ScrollView>
-    </ScreenWrapper>
+
+        <CarpoolCard
+          profileImage="https://randomuser.me/api/portraits/men/32.jpg"
+          from="Amsterdam"
+          to="Utrecht"
+          time="08:00 - 09:00"
+        />
+      </Card>
+    </ScrollView>
   );
 }
