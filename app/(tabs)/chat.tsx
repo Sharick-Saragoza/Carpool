456109f5837bca92d45a52a041642da5cb92b68b
Hamid-xb <120631726+Hamid-xb@users.noreply.github.com>
2025-11-07 14:31:24 +0100

    Remove ScreenWrapper component and simplify ChatScreen and ExploreScreen layouts

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,7 @@
-import ScreenWrapper from '@/components/ScreenWrapper';
 import { Text } from 'react-native';
 
 export default function ChatScreen() {
   return (
-    <ScreenWrapper>
-      <Text className="">Chat Screen</Text>
-    </ScreenWrapper>
+    <Text className="">Chat Screen</Text>
   );
 }
