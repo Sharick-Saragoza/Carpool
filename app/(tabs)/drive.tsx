fb82faad01604d655803f9e54b8fedeee9e952a7
Gamzee <lithiumturkin@protonmail.com>
2025-11-17 10:54:15 +0100

    Fix: Add missing imports

--- app/(tabs)/drive.tsx
+++ app/(tabs)/drive.tsx
@@ -1,5 +1,6 @@
 import { router } from 'expo-router';
 import { Plus } from 'lucide-react-native';
+import { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CarpoolCard } from '@/components/CarpoolCard';
